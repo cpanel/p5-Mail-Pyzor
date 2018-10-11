0b182b0983562a149cb15e09798706b3cde5bfe2
Felipe Gasper <felipe@cpanel.net>
2018-10-11 12:58:55 -0500

    main file

---
+++ lib/Mail/Pyzor.pm
@@ -0,0 +1,41 @@
+package Mail::Pyzor;
+
+# Copyright (c) 2012, cPanel, LLC.
+# All rights reserved.
+# http://cpanel.net
+#
+# This is free software; you can redistribute it and/or modify it under the
+# same terms as Perl itself. See L<perlartistic>.
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01-TRIAL1';
+
+=encoding utf-8
+
+=head1 NAME
+
+Mail::Pyzor - Pyzor spam filtering in Perl
+
+=head1 DESCRIPTION
+
+This distribution contains Perl implementations of parts of
+L<Pyzor|http://pyzor.org>, a tool for use in spam email filtering.
+It is intended for use with L<Mail::SpamAssassin> but may be useful
+in other contexts.
+
+See the following modules for information on specific tools that
+the distribution includes:
+
+=over
+
+=item * L<Mail::Pyzor::Client>
+
+=item * L<Mail::Pyzor::Digest>
+
+=back
+
+=cut
+
+1;
